c7468e49ce9860760fa2a6a526913987f8fef6dd
Mariusz Wincior <Mariusz Wincior@DESKTOP-17H8CVO>
2018-03-17 20:56:50 +0100

    gf

--- domobridge.py
+++ domobridge.py
@@ -5,6 +5,9 @@ Created on 17 mar 2018
 '''
 import urllib.request
 import json
+import codecs
+
+
 DOMO_IP = "192.168.1.100"
 DOMO_PORT = "8050"
 
@@ -12,9 +15,8 @@ def domoticz_getJson(idx):
     url = "http://"+DOMO_IP+":"+DOMO_PORT+"/json.htm?type=devices&rid="
     req = urllib.request.Request(url+str(idx))
     response = urllib.request.urlopen(req)
-
-    # Parse Json
-    data = json.load(response)
+    reader = codecs.getreader("utf-8")
+    data = json.load(reader(response))
     return data
 
 def domoticz_set_value(idx,value):
