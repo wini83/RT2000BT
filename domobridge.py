d8aaf6c07bade5dad01cdfb42da83e5223da04dd
Mariusz Wincior <Mariusz Wincior@DESKTOP-17H8CVO>
2018-03-17 20:31:41 +0100

    domobridge+

--- domobridge.py
+++ domobridge.py
@@ -17,6 +17,15 @@ def domoticz_getJson(idx):
     data = json.load(response)
     return data
 
+def domoticz_set_value(idx,value):
+    url = "http://"+DOMO_IP+":"+DOMO_PORT+"/json.htm?type=command&param=udevice&idx="+str(idx)+"&nvalue=0&svalue="+str(value)
+    req = urllib.request.Request(url)
+    response = urllib.request.urlopen(req)
+
+    # Parse Json
+    data = json.load(response)
+    return data
+
 
 def is_Switch_On(idx):
     status_idx = domoticz_getJson(idx)
@@ -30,7 +39,34 @@ def is_Switch_On(idx):
     except:
         return False
     
+def print_raw_json(status_idx):
+    print(json.dumps(status_idx, indent=4, sort_keys=True))
 
-dupa =  is_Switch_On(2132)
-
-print(dupa)
+def read_temp(idx):
+    status_idx = domoticz_getJson(idx)
+    try:
+        dev_temp = status_idx["result"][0]["Temp"]
+        return dev_temp
+    except:
+        return -255
+    
+def read_SetPoint(idx):
+    status_idx = domoticz_getJson(idx)
+    try:
+        dev_temp = status_idx["result"][0]["SetPoint"]
+        return dev_temp
+    except:
+        return -255
+    
+def set_temp(idx,value):
+    status_idx = domoticz_set_value(idx, value)
+    
+    try:
+        dev_status = status_idx["status"]
+        if dev_status == "OK":
+            return True
+        else:
+            return False
+    except:
+        return False
+    
