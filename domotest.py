3502484cbe90d06f9f00cee593d7d6b807c191b6
Mariusz Wincior <Mariusz Wincior@DESKTOP-17H8CVO>
2018-03-17 20:51:55 +0100

    d

---
+++ domotest.py
@@ -0,0 +1,10 @@
+'''
+Created on 17 mar 2018
+
+@author: Mariusz Wincior
+'''
+import domobridge
+ist = 17.0
+soll = 23.0
+print(domobridge.set_temp(1802, ist))
+print(domobridge.set_temp(1801, soll))
