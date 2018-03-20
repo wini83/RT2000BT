6250ad7b626d95971842d5936594516c1fb7a841
Mariusz Wincior <Mariusz Wincior@DESKTOP-17H8CVO>
2018-03-20 22:13:40 +0100

    float

--- domobridge.py
+++ domobridge.py
@@ -88,6 +88,7 @@ def read_SetPoint(idx):
     try:
         dev_temp = status_idx["result"][0]["SetPoint"]
         print(dev_temp)
+        dev_temp_int = float(dev_temp)
         dev_temp_int = int(dev_temp)
         return dev_temp_int
     except:
