06a926300959ee64aec43f7594cc929c70b37505
wini83 <mariusz.wincior@gmail.com>
2018-09-03 18:32:16 +0200

    ds

--- readdevice.py
+++ readdevice.py
@@ -6,7 +6,6 @@ Created on 17 mar 2018
 
 import gatt
 import rt2000BT
-# from rt2000BT.downloadDevice import downloadDevice
 
 SETTEMP_IDX = 4094
 TEMPACT_IDX = 1802
@@ -23,5 +22,5 @@ manager = gatt.DeviceManager(adapter_name='hci0')
 device = rt2000BT.downloadDevice(mac_address='9E:5F:48:89:87:D5', manager=manager)
 device.connect()
 manager.run()
-print ("dupa")
-print(device.battery)
+if(device.downloadDevice.is_download_succesful):
+    print("dupa")
