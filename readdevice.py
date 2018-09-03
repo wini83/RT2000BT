df90f2eac8c22632e48182e27248321e5fcd5995
wini83 <mariusz.wincior@gmail.com>
2018-09-03 22:11:14 +0200

    k

--- readdevice.py
+++ readdevice.py
@@ -17,7 +17,6 @@ DOMOTICZ_IP = "192.168.1.100"
 DOMOTICZ_PORT = "8050"
 
 
-
 manager = gatt.DeviceManager(adapter_name='hci0')
 valve = rt2000BT.downloadDevice(mac_address='9E:5F:48:89:87:D5', manager=manager)
 valve.connect()
@@ -25,19 +24,11 @@ manager.run()
 print("-------------------------------------------------")
 if(valve.is_download_succesful):    
     print("Battery = " + str(valve.battery))
-    if(valve.mode_auto == 0):
-        print("Auto Mode")
-    elif(valve.mode_auto == 1):
-        print("Manual Mode")
-    else:
-        print("Wrong Mode")
     server = dom.Server(address=DOMOTICZ_IP, port=DOMOTICZ_PORT)
     dev_act = dom.Device(server,TEMPACT_IDX)
     dev_set = dom.Device(server,SETPOINT_IDX)
     dev_mode =  dom.Device(server,MANUAL_IDX)
     print("Current Temp: domoticz={}C; valve ={}C".format(dev_act.temp,valve.current_temp))
-    print(type(dev_act.temp))
-    print(type(valve.current_temp))
     if(valve.current_temp != dev_act.temp):
         print("Updating Current Temp in domoticz...")
         dev_act.update(0, "{}&battery={}".format(valve.current_temp,valve.battery))
@@ -45,4 +36,19 @@ if(valve.is_download_succesful):
     if(valve.setpoint_temp != dev_set.temp):
         print("Updating Setpoint in domoticz...")
         dev_set.update(0, "{}&battery={}".format(valve.setpoint_temp,valve.battery))
-        
+    if(valve.mode_auto == 0):
+        if(dev_mode.data == "Off"):
+            print("MODE: domoticz=auto; valve=auto")
+        else:
+            print("MODE: domoticz=manual; valve=auto")
+            print("Updating MODE in domoticz...")
+            dev_mode.updateSwitch("Off")
+    elif(valve.mode_auto == 1):
+        if(dev_mode.data == "On"):
+            print("MODE: domoticz=manual; valve=manual")
+        else:
+            print("Mode: domoticz=auto; valve=manual")
+            print("Updating Mode in domoticz...")
+            dev_mode.updateSwitch("On")
+    else:
+        print("Wrong Mode")
