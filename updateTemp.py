58b5cb2ae7e1ed1633e3fec503d6d3d714c49526
wini83 <mariusz.wincior@gmail.com>
2019-08-24 23:26:23 +0200

    go

--- updateTemp.py
+++ updateTemp.py
@@ -82,7 +82,7 @@ class AnyDevice(gatt.Device):
 			if(settemp != -255):
 				if(settemp>15 and settemp < 30):
 					if(int(settemp*2) != self.bajty[1]):
-						self.bajty[1]=settemp*2
+						self.bajty[1]=int(settemp*2)
 						print("new values:")
 						print(self.bajty, end=" ")
 						device_information_service = next(
