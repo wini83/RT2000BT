d7a83774a488d04715cca19b88af31d379e3091b
wini83 <mariusz.wincior@gmail.com>
2020-04-04 11:47:59 +0200

    poll reformat

--- poll.py
+++ poll.py
@@ -1,4 +1,3 @@
-
 import rt2000BT
 import DomoticzAPI as dom
 
@@ -14,29 +13,29 @@ if valve.is_polling_successful:
     server = dom.Server(address=config.domoticz_ip, port=config.domoticz_port)
     dev_act = dom.Device(server, config.temp_current_idx)
     dev_set = dom.Device(server, config.setpoint_idx)
-    dev_mode =  dom.Device(server, config.manual_idx)
+    dev_mode = dom.Device(server, config.manual_idx)
     print("Current Temp: domoticz={}C; valve ={}C".format(dev_act.temp, valve.current_temp))
     if valve.current_temp != dev_act.temp:
         print("Updating Current Temp in domoticz...")
-        dev_act.update(0, "{}&battery={}".format(valve.current_temp, valve.battery))
-    print("Setpoint: domoticz={}C; valve ={}C".format(dev_set.temp, valve.setpoint_temp))
-    if valve.setpoint_temp != dev_set.temp:
+        dev_act.update(0, valve.current_temp, valve.battery, None)
+    print("Setpoint: domoticz={}C; valve ={}C".format(dev_set.temp, valve.set_point_temp))
+    if valve.set_point_temp != dev_set.temp:
         print("Updating Setpoint in domoticz...")
-        dev_set.update(0, "{}&battery={}".format(valve.setpoint_temp, valve.battery))
+        dev_set.update(0, valve.set_point_temp, valve.battery, None)
     if valve.mode_auto == 0:
         if dev_mode.data == "Off":
             print("MODE: domoticz=auto; valve=auto")
         else:
             print("MODE: domoticz=manual; valve=auto")
             print("Updating MODE in domoticz...")
-            dev_mode.updateSwitch("Off")
+            dev_mode.update_switch("Off")
     elif valve.mode_auto == 1:
         if dev_mode.data == "On":
             print("MODE: domoticz=manual; valve=manual")
         else:
             print("Mode: domoticz=auto; valve=manual")
             print("Updating Mode in domoticz...")
-            dev_mode.updateSwitch("On")
+            dev_mode.update_switch("On")
     else:
         print("Wrong Mode")
 else:
