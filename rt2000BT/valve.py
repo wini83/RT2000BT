297361ce2f9561c3f7d8c5679d13d40845f15cce
wini83 <mariusz.wincior@gmail.com>
2020-04-04 10:59:15 +0200

    poll rename

--- rt2000BT/valve.py
+++ rt2000BT/valve.py
@@ -1,6 +1,4 @@
-import struct
-import sys
-from enum import Enum
+import pygatt
 
 SERVICE_ID = "47e9ee00-47e9-11e4-8939-164230d1df67"
 
@@ -14,33 +12,32 @@ BATTERY_ID_ALt = "47e9ee2c-47e9-11e4-8939-164230d1df67"
 
 PIN_ID = "47e9ee30-47e9-11e4-8939-164230d1df67"
 
-Modus = Enum("Modus", "unknown poll update_temp set_mode")
 
+class Valve:
 
-class ValveAdapter:
-    _is_settings_acquired = False
-    _is_status_acquired = False
-    _is_battery_acquired = False
-    adapter = None
-    mac = ""
-    battery = 255
-    current_temp = 0
-    set_point_temp = 0
-    mode_auto = -1
-    desired_temp = 0
-    desired_mode = False
-    is_polling_successful = False
-    mode = Modus.unknown
+    def __init__(self, mac, pin):
+        self.adapter = pygatt.GATTToolBackend()
+        self._is_settings_acquired = False
+        self._is_status_acquired = False
+        self._is_battery_acquired = False
+        self.mac = mac
+        self.pin = pin
+        self.battery = 255
+        self.current_temp = 0
+        self.set_point_temp = 0
+        self.mode_auto = -1
+        self.desired_temp = 0
+        self.is_polling_successful = False
 
-
-
-    def poll(self):
-        self.mode = Modus.poll
-        super().connect()
-        self.manager.run()
-
-    def set_mode(self, value=False):
-        self.mode = Modus.set_mode
-        self.desired_mode = value;
-        super().connect()
-        self.manager.run()
+        def poll(self):
+            try:
+                self.adapter.start()
+                device = self.adapter.connect(self.mac)
+                device.char_write(PIN_ID, bytearray(b'\x00\x00\x00\x00'))
+                settings = list(device.char_read(SETTINGS_ID))
+                self.current_temp = settings[0] / 2
+                self.set_point_temp = settings[1] / 2
+                self.battery = list(device.char_read(BATTERY_ID_ALt))[0]
+                self.is_polling_successful = True
+            finally:
+                self.adapter.stop()
