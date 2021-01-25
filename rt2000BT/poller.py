b22e8c1b3ab74dc87ef6007cc298175ae1633454
wini83 <mariusz.wincior@gmail.com>
2021-01-25 22:11:24 +0100

    logging + mqtt3

--- rt2000BT/poller.py
+++ rt2000BT/poller.py
@@ -12,19 +12,22 @@ def poll_valve(valve, client: mqtt.Client):
 
     if is_polling_ok:
         logging.info("Battery = %s", valve.battery)
-        client.publish("{}/State".format(config.mqtt_topic), payload=valve.battery, qos=0, retain=False)
+        client.publish("{}/Battery".format(config.mqtt_topic), payload=valve.battery, qos=0, retain=False)
         server = dom.Server(address=config.domoticz_ip, port=config.domoticz_port)
         dev_act = dom.Device(server, config.temp_current_idx)
         dev_set = dom.Device(server, config.setpoint_idx)
         dev_mode = dom.Device(server, config.manual_idx)
         print("Current Temp: domoticz={}C; valve ={}C".format(dev_act.temp, valve.current_temp))
+        client.publish("{}/Temp-current".format(config.mqtt_topic), payload=valve.current_temp, qos=0, retain=False)
         if valve.current_temp != dev_act.temp:
             print("Updating Current Temp in domoticz...")
             dev_act.update(0, valve.current_temp, valve.battery, None)
         print("Setpoint: domoticz={}C; valve ={}C".format(dev_set.temp, valve.set_point_temp))
+        client.publish("{}/Temp-setpoint".format(config.mqtt_topic), payload=valve.set_point_temp, qos=0, retain=False)
         if valve.set_point_temp != dev_set.temp:
             print('Updating Setpoint in domoticz...')
             dev_set.update(0, valve.set_point_temp, valve.battery, None)
+        client.publish("{}/Auto-Mode".format(config.mqtt_topic), payload=valve.mode_auto, qos=0, retain=False)
         if valve.mode_auto == 0:
             if dev_mode.data == "Off":
                 print("MODE: domoticz=auto; valve=auto")
