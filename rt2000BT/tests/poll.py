9b625e5b42d2394e6b00db5efb84309d97128231
wini83 <mariusz.wincior@gmail.com>
2026-02-22 22:02:42 +0100

    build:cleaning the repo

--- rt2000BT/tests/poll.py
+++
@@ -1,8 +0,0 @@
-import rt2000BT
-
-import config
-from rt2000BT import poller
-
-valve = rt2000BT.Valve(config.mac, None)
-
-poller.poll_valve(valve)
--- rt2000BT/tests/test_worker.py
+++
@@ -1,5 +0,0 @@
-from worker import Worker
-
-worker1 = Worker()
-
-worker1.run()
